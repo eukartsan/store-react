e3fbbb6b78dde84a78e7db2913360da4e6d6f21e
eukartsan <eugenekartsan@gmail.com>
2019-01-31 13:36:40 +0200

    add spinner and links

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,20 +1,25 @@
 export default class BookstoreService {
 
-    getBooks() {
-        return [
-            {
-                id: 1,
-                title: '1984',
-                author: 'George Orwell',
-                price: 30,
-                coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'},
-            {
-                id: 2,
-                title: 'Dracula',
-                author: 'Bram Stoker',
-                price: 45,
-                coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'}
-        ];
-    }
+    data = [
+        {
+            id: 1,
+            title: '1984',
+            author: 'George Orwell',
+            price: 30,
+            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'},
+        {
+            id: 2,
+            title: 'Dracula',
+            author: 'Bram Stoker',
+            price: 45,
+            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'}
+    ]
 
+    getBooks() {
+        return  new Promise((resolve) => {
+            setTimeout(() => {
+                resolve(this.data)
+            }, 600)
+        })
+    }
 }
