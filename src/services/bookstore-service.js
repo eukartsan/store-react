f98a2ac7bc651027334b70f86ad70f98f42b726f
eukartsan <eugenekartsan@gmail.com>
2019-02-01 15:51:22 +0200

    add error indicator

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -6,17 +6,19 @@ export default class BookstoreService {
             title: '1984',
             author: 'George Orwell',
             price: 30,
-            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'},
+            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'
+        },
         {
             id: 2,
             title: 'Dracula',
             author: 'Bram Stoker',
             price: 45,
-            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'}
+            coverImage: 'https://imgc.allpostersimages.com/img/print/u-g-F5TYBR0.jpg'
+        }
     ]
 
     getBooks() {
-        return  new Promise((resolve) => {
+        return new Promise((resolve) => {
             setTimeout(() => {
                 resolve(this.data)
             }, 600)
