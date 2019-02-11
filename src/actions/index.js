814f47c0c539e65570ea6c27ce854c066447d680
eukartsan <eugenekartsan@gmail.com>
2019-02-11 17:20:48 +0200

    add edit and del

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,30 +1,51 @@
-const booksLoaded = (newBooks) => {
+const booksRequested = () => {
     return {
-        type: 'BOOKS_LOADED',
-        payload: newBooks
+        type: 'FETCH_BOOKS_REQUEST'
     }
 };
 
-const booksRequested = () => {
+const booksLoaded = (newBooks) => {
     return {
-        type: 'BOOKS_REQUESTED'
-    }
+        type: 'FETCH_BOOKS_SUCCESS',
+        payload: newBooks
+    };
 };
 
 const booksError = (error) => {
     return {
-        type: 'BOOKS_ERROR',
+        type: 'FETCH_BOOKS_FAILURE',
         payload: error
-    }
+    };
+};
+
+export const bookAddedToCart = (bookId) => {
+    return {
+        type: 'BOOK_ADDED_TO_CART',
+        payload: bookId
+    };
+};
+
+export const bookRemovedFromCart = (bookId) => {
+    return {
+        type: 'BOOK_REMOVED_FROM_CART',
+        payload: bookId
+    };
+};
+
+export const allBooksRemovedFromCart = (bookId) => {
+    return {
+        type: 'ALL_BOOKS_REMOVED_FROM_CART',
+        payload: bookId
+    };
 };
 
 const fetchBooks = (bookstoreService, dispatch) => () => {
-    dispatch(booksRequested())
+    dispatch(booksRequested());
     bookstoreService.getBooks()
-    .then((data) => dispatch(booksLoaded(data)))
-    .catch((err) => dispatch(booksError(err)))
-}
+        .then((data) => dispatch(booksLoaded(data)))
+        .catch((err) => dispatch(booksError(err)));
+};
 
 export {
     fetchBooks
-}
+};
